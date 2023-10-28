ba67afe77cbe91e5da3f43fc3db1e16b49219761
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-28 16:45:14 -0600

    Initial commit

---
+++ challenges.rb
@@ -0,0 +1,32 @@
+# Challenge 1
+# Write a method or function that accepts a string. The method or function should return a boolean that describes whether or not the string has an even number of characters. 
+
+Text. 
+
+
+# Challenge 2
+# Update your previous method or function so it accepts an array. The method or function should return a boolean that describes whether or not the array's length is an even number.
+
+Text. 
+
+
+# Challenge 3
+# Write a method or function that takes in an argument of a number that represents the current time to the closest hour in 24-hour military time. (i.e. 11 = 11:00 AM, 16 = 4:00 PM). The function should return a different greeting based on the time of day.
+
+4 AM - 11 AM: Good morning!
+12 PM - 4 PM: Good afternoon!
+5 PM - 8PM: Good evening!
+9 PM - 3 AM: Good night! 
+
+
+# Challenge 4
+# Write a method or function that accepts an array of 10 integers (between 0 and 9) and returns a string of those numbers in the form of a phone number. For example, if you were given [5, 5, 5, 1, 2, 3, 4, 5, 6, 7], the method or function will return 555-123-4567.
+
+Text. 
+
+
+#Challenge 5 - OPTIONAL! Super Spicy!
+#Start with the array of strings below. Print all of the words in the array, but change every t to an uppercase T.
+
+animals: ['cat', 'dog', 'parrot', 'turtle', 'bird'] 
+
