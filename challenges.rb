1185290a436088e1437e9d78224bacb24f14cff1
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-29 21:00:59 -0600

    Start challenge four

--- challenges.rb
+++ challenges.rb
@@ -45,7 +45,11 @@ end
 # Challenge 4
 # Write a method or function that accepts an array of 10 integers (between 0 and 9) and returns a string of those numbers in the form of a phone number. For example, if you were given [5, 5, 5, 1, 2, 3, 4, 5, 6, 7], the method or function will return 555-123-4567.
 
-Text. 
+numbers = [1, 2, 3, 5, 5, 5, 1, 2, 3, 4]
+
+def phone_digits(phone) 
+  p "Phone Number: (numbers.fetch(0, 1, 2)) numbers.fetch(3, 4, 5) - numbers.fetch(6, 7, 8, 9)"
+end
 
 
 # Challenge 5 - OPTIONAL! Super Spicy!
